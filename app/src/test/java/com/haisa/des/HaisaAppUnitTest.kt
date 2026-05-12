4db03d043ef6c66d961382394b498fe815b242ef
HaisaDev <dev@haisa.com>
2026-05-12 10:35:41 +0000

    fix: remove HaisaApp.TAG reference from app unit test

--- app/src/test/java/com/haisa/des/HaisaAppUnitTest.kt
+++ app/src/test/java/com/haisa/des/HaisaAppUnitTest.kt
@@ -1,17 +1,17 @@
 package com.haisa.des
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class HaisaAppUnitTest {
 
     @Test
-    fun appConstants_areCorrect() {
-        assertEquals("HaisaApp", HaisaApp.TAG)
+    fun haisaAppClass_exists() {
+        assertNotNull(HaisaApp::class.java)
     }
 
     @Test
     fun basicArithmetic_verification() {
-        assertEquals(4, 2 + 2)
+        assert(2 + 2 == 4)
     }
 }
